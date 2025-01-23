b17254c529e28370ca19fbc17adf4320f8021bd3
diavor01 <darius.iavorschi@gmail.com>
2025-01-23 00:42:25 -0500

    Update read_num_char.c
    NOTE: NONE OF THE MODIFICATIONS I MADE SO FAR HAS BEEN TESTED. THEY MAY VERY WELL BE WRONG

--- read_num_char.c
+++ read_num_char.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "list.h"
 
-void read_num_char(char* line, /*matrix of numbers, list of non-nums*/)
-{
-    while ((ch = fgetc(line)) != '\n')
-    {
-        if (isdigit(ch))
-            //nums[i][j] = ch;
-        else
-            //nonnums[i][j] = ch;
+void read_num_char(const char *line, List_T *matrix_nums, List_T *list_chars) {
+    List_T row = NULL;
+
+    size_t capacity = 1001;
+    size_t size = 0;
+    char *line_chars = malloc(capacity);
+
+    if (!line_chars) {
+        perror("Failed to allocate memory for line_chars");
+        exit(1);
     }
+
+    char ch;
+    while ((ch = *line++) != '\n') 
+    {
+        if (isdigit(ch)) {
+            char *num = malloc(sizeof(char));
+            if (!num) {
+                perror("Failed to allocate memory for num");
+                exit(1);
+            }
+            *num = ch;
+            row = List_push(row, num);
+        } 
+        else 
+        {
+            line_chars[size++] = ch;
+        }
+    } 
+
+    line_chars[size] = '\0';
+
+    *matrix_nums = List_push(*matrix_nums, List_reverse(row));
+    *list_chars = List_push(*list_chars, line_chars);
 }
-
-
 
 tuple check_equal_lines(/*list of non-nums*/)
 {
