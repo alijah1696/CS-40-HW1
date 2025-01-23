11490c5e20b07875d419c1b18c71e842b7ee4a9c
diavor01 <darius.iavorschi@gmail.com>
2025-01-23 12:57:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "readaline.h"
-#include "list.h"
-
+//#include "list.h"
 
 int main(int argc, char *argv[])
 {
-
-
+    
     if (argc == 2)
     {
         FILE *file = fopen(argv[1], "r");
@@ -16,27 +14,14 @@ int main(int argc, char *argv[])
             perror("Error opening file");
             return 1;
         }
-        char* line;
-        int ok = 0;
+        char* line = NULL;
+        // int ok = 0;
 
-        while (readaline(&file, &*line) > 0)
+        while (readaline(file, &line) > 0)
         {
-            if (ok == 0)
-            {
-                read_num_char(line, &matrix numbers, &list nonnums)
-                if (check_equal_lines != (-1, -1))
-                {
-                    //delete all lines that are not identical
-                    ok = 1;
-                } 
-            }
-            else
-            {
-                //just check if the nonnums are equal to the verified line. If they are not, then do not append the numbers to the matrix
-
-            }
-            
+            printf("%s\n", line);
+            free(line);
         }
-        
+        fclose(file);
     }
 }
