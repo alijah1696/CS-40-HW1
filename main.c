4d4909c4adf40e974947d8ec2f8ac60ca343c63e
diavor01 <darius.iavorschi@gmail.com>
2025-01-26 18:08:35 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "restoration.h"
 #include "readaline.h"
-//#include "list.h"
+#include "seq,h"
 
 int main(int argc, char *argv[])
 {
-    
-    if (argc == 2)
+    if (argc == 3)
     {
         FILE *file = fopen(argv[1], "r");
         
@@ -14,14 +15,70 @@ int main(int argc, char *argv[])
             perror("Error opening file");
             return 1;
         }
+
         char* line = NULL;
-        // int ok = 0;
+        Seq_T matrix = Sequence_new(150);
+        Seq_T atom_sequence = Seq_new(0);
+        int ok = 0;
+        int width = 0;
+        int heigth = 0;
+        int false_lines = 0;
+        int total_corrupted_lines = 0;
 
         while (readaline(file, &line) > 0)
-        {
-            printf("%s\n", line);
-            free(line);
+        {    
+            printf(line);
         }
-        fclose(file);
     }
+    else
+        printf(stderr, "Wrong number of arguments provided");
 }
+
+// {
+    
+//     if (argc == 2)
+//     {
+//         FILE *file = fopen(argv[1], "r");
+        
+//         if (file == NULL) {
+//             perror("Error opening file");
+//             return 1;
+//         }
+
+//         char* line = NULL;
+//         Seq_T matrix = Sequence_new(150);
+//         Seq_T atom_sequence = Seq_new(0);
+//         int ok = 0;
+//         int width = 0;
+//         int heigth = 0;
+//         int false_lines = 0;
+//         int total_corrupted_lines = 0;
+
+//         while (readaline(file, &line) > 0)
+//         {
+//             total_corrupted_lines++;
+//             if (ok == 0)
+//             {
+//                 int index = diff_nums_chars1(line, &matrix, &atom_sequence);
+//                 if (index != -1)
+//                 {
+//                     const char* correct_atom = Seq_get(atom_sequence, index);
+//                     int size_matrix = Seq_length(matrix);
+//                     false_lines = Seq_length(matrix) - 2;
+//                     matrix = correct_matrix(matrix, size_matrix, index);
+//                     ok = 1;
+//                 }
+//             }
+//             else
+//             {
+//                 false_lines+= diff_nums_chars2(line, correct_atom, matrix, atom_sequence, width);
+//             }
+
+//             free(line);
+//         }
+
+//         assert(total_corrupted_lines - Seq_length(matrix) == failed_lines);
+//         fclose(file);
+//         create_P2_file(matrix);
+//     }
+// }
--- restoration.c
+++ restoration.c
@@ -1,23 +1,159 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
+
 #include "atom.h"
-#include "conversion.h"
+#include "seq.h"
+#include "restoration.h"
 
 
-void create_array_atoms()
+//It is known that the lines of text have less than 1000 characters
+int diff_nums_chars1(const char *line, Seq_T matrix, Seq_T atom_sequence)
 {
-    const char *atoms[2];
+    assert(line != NULL);
 
-    atoms[0] = Atom_string("hey");
-    atoms[1] = Atom_string("hello");
+    int capacity = 1001;
+    Seq_T newRow = Seq_new(capacity);
+    char* atom_val = malloc(capacity);
 
-    printf("%s\n", atoms[0]);
-    printf("%s\n", atoms[1]);
+    if (!atom_val) {
+        perror("Failed to allocate memory for atom_val");
+        exit(1);
+    }
+
+    separate(line, newRow, atom_val);
+
+    Seq_addhi(matrix, newRow);
+
+    const char *atom = Atom_string(atom_val);
+    Seq_addhi(atom_sequence, atom);
+
+    free(atom_val);
+
+    return check_atoms(atom_sequence, atom);
 }
 
-int main()
+
+int diff_nums_chars2(const char* line, char* correct_atom, Seq_T matrix, 
+                                                Seq_T atom_sequence, int width)
 {
-  create_array_atoms();
-  exampleConversion();
+    assert(line != NULL);
+    assert(correct_atom != NULL);
+
+    int capacity = 1001;
+    Seq_T newRow = Seq_new(capacity);
+    char* atom_val = malloc(capacity);
+    int fl = 0;
+
+
+    if (!atom_val) {
+        perror("Failed to allocate memory for atom_val");
+        exit(1);
+    }
+
+    separate(line, newRow, atom_val);
+
+    const char *atom = Atom_string(atom_val);
+
+    if (atom == correct_atom)
+    {
+        assert(Seq_length(newRow) == width);
+
+        Seq_addhi(atom_sequence, atom);
+        Seq_addhi(matrix, newRow);
+    }
+    else
+        fl++;
+
+    free(atom_val);
+
+    return fl;
 }
+
+void separate(const char* line, Seq_T newRow, char* atom_val)
+{
+    assert(line != NULL);
+    assert(atom_val != NULL);
+
+    char ch;
+    size_t size_atom = 0;
+    int capacity = 1001;  // Define capacity to enforce boundary checks
+
+    while ((ch = *line++) != '\0')
+    {
+        if (isdigit((unsigned char)ch)) 
+        {
+            int* num = malloc(sizeof(int));  // Allocate memory for an int
+            if (!num) 
+            {
+                perror("Failed to allocate memory for num");
+                exit(1);
+            }
+            *num = ch - '0';
+            Seq_addhi(newRow, num);
+        } 
+        else 
+        {
+            if (size_atom >= (size_t)(capacity - 1)) 
+            {  
+                fprintf(stderr, "atom_val buffer overflow\n");
+                exit(1);
+            }
+            atom_val[size_atom++] = ch;
+        }
+    }
+    atom_val[size_atom] = '\0';
+}
+
+
+int check_atoms(Seq_T atom_sequence, const char* atom)
+{
+    
+    for (int i = 0; i < Seq_length(atom_sequence); i++) {
+        const char *current_atom = (const char *)Seq_get(atom_sequence, i);
+        if (current_atom == atom)
+            return i;
+    }
+    return -1;
+}
+
+Seq_T correct_matrix(Seq_T matrix, int size_matrix, int index)
+{
+    if (index < 0 || index >= size_matrix) {
+        fprintf(stderr, "Index %d out of bounds for matrix of size %d\n", index, size_matrix);
+        exit(1);
+    }
+
+    if (size_matrix <= 0) {
+        fprintf(stderr, "Invalid size_matrix: %d\n", size_matrix);
+        exit(1);
+    }
+
+    Seq_T new_matrix = 
+            Seq_seq(Seq_get(matrix, index), Seq_get(matrix, size_matrix - 1), NULL);
+    return new_matrix;
+} 
+
+// For testing purposes
+void create_P2_file(Seq_T matrix)
+{
+    for (size_t i = 0; i < (size_t)Seq_length(matrix); i++)  // Cast to size_t if Seq_length returns int
+    {
+        Seq_T row = (Seq_T)Seq_get(matrix, i);
+        for (size_t j = 0; j < (size_t)Seq_length(row); j++) 
+        {
+            int *num_ptr = (int *)Seq_get(row, j);
+            if (num_ptr) {
+                printf("%d ", *num_ptr);
+            } else {
+                printf("NULL ");
+            }
+        }
+        printf("\n");  // Newline after each row
+    }
+    printf("create_P2_file was successful\n");
+}
+
+
+
