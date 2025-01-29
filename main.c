461b05b5df0d3931bd3a1dc4d9b5ec9e16c8212b
diavor01 <darius.iavorschi@gmail.com>
2025-01-29 13:30:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,82 +3,118 @@
 
 #include "restoration.h"
 #include "readaline.h"
-#include "seq,h"
+#include "seq.h"
+#include "atom.h"
+#include "unit_tests.h"
 
-int main(int argc, char *argv[])
+// // The lines are not supposed to end with \0 because then len is going to be 1 higher 
+
+
+
+// int main()
+// {
+//     test_separate_extended();
+//     return 0;
+// }
+
+int main(int argc, char *argv[]) 
 {
-    if (argc == 3)
+    if (argc == 2)
     {
-        FILE *file = fopen(argv[1], "r");
-        
-        if (file == NULL) {
-            perror("Error opening file");
+        FILE *file = fopen(argv[1], "rb");
+
+        if (!file) {
+            perror("fopen");
             return 1;
         }
+        const char* correct_atom = NULL;
 
-        char* line = NULL;
-        Seq_T matrix = Sequence_new(150);
+        char *line = NULL;
+        size_t len;
+        Seq_T matrix = Seq_new(0);
         Seq_T atom_sequence = Seq_new(0);
         int ok = 0;
         int width = 0;
-        int heigth = 0;
+        // int heigth = 0;
         int false_lines = 0;
         int total_corrupted_lines = 0;
 
-        while (readaline(file, &line) > 0)
-        {    
-            printf(line);
+        while ((len = readaline(file, &line)) > 0) 
+        {
+            printf("\n");
+            printf("len: %zu", len);
+            printf("\n");
+            printf("Current line\n");
+            printf("%s\n", line);
+            total_corrupted_lines++;
+            if (ok == 0)
+            {
+                int index = diff_nums_chars1(line, matrix, atom_sequence);
+                if (index != -1)
+                {
+                    printf("The index is: ");
+                    printf("%d", index);
+                    printf("\n");
+
+                    correct_atom = (char*) Seq_get(atom_sequence, index);
+
+                    int size_matrix = Seq_length(matrix);
+                    false_lines = Seq_length(matrix) - 2;
+
+                    matrix = correct_matrix(matrix, size_matrix, index);
+
+                    printf("Seq_length(Seq_get(matrix, 0)): %d\n", Seq_length(Seq_get(matrix, 0)));
+                    assert(Seq_length(Seq_get(matrix, 0)) == Seq_length(Seq_get(matrix, 1)));
+                    width = Seq_length(Seq_get(matrix, 0));
+
+                    printf("%s", "The matrix after discovering the first correct 2 indices\n");
+                    printing_matrix(matrix);
+                    printf("\n");
+
+                    ok = 1;
+                }
+                else
+                {
+                    printf("Have not discovered the correct 2 indices yet\n");
+                }
+            }
+            else
+            {
+                false_lines+= diff_nums_chars2(line, correct_atom, matrix, atom_sequence, width);
+            }
+
+            free(line);
         }
+
+        printf("%d", total_corrupted_lines);
+        printf("\n");
+        printf("%d", Seq_length(matrix));
+        printf("\n");
+        printf("%d", false_lines);
+        printf("\n");
+
+        assert(total_corrupted_lines - Seq_length(matrix) == false_lines);
+        fclose(file);
+
+        printf("\n");
+        printf("%s", "Final matrix\n");
+        printing_matrix(matrix);
+
+        for (size_t i = 0; i < (size_t)Seq_length(matrix); i++){
+            Seq_free((Seq_T)Seq_get(matrix, i));
+        }
+
+        Seq_free(matrix);
+        Seq_free(atom_sequence);
+
+
+        return 0;
     }
     else
-        printf(stderr, "Wrong number of arguments provided");
+    {
+        printf("%d", argc);
+        return 1;
+    }
 }
 
-// {
-    
-//     if (argc == 2)
-//     {
-//         FILE *file = fopen(argv[1], "r");
-        
-//         if (file == NULL) {
-//             perror("Error opening file");
-//             return 1;
-//         }
 
-//         char* line = NULL;
-//         Seq_T matrix = Sequence_new(150);
-//         Seq_T atom_sequence = Seq_new(0);
-//         int ok = 0;
-//         int width = 0;
-//         int heigth = 0;
-//         int false_lines = 0;
-//         int total_corrupted_lines = 0;
-
-//         while (readaline(file, &line) > 0)
-//         {
-//             total_corrupted_lines++;
-//             if (ok == 0)
-//             {
-//                 int index = diff_nums_chars1(line, &matrix, &atom_sequence);
-//                 if (index != -1)
-//                 {
-//                     const char* correct_atom = Seq_get(atom_sequence, index);
-//                     int size_matrix = Seq_length(matrix);
-//                     false_lines = Seq_length(matrix) - 2;
-//                     matrix = correct_matrix(matrix, size_matrix, index);
-//                     ok = 1;
-//                 }
-//             }
-//             else
-//             {
-//                 false_lines+= diff_nums_chars2(line, correct_atom, matrix, atom_sequence, width);
-//             }
-
-//             free(line);
-//         }
-
-//         assert(total_corrupted_lines - Seq_length(matrix) == failed_lines);
-//         fclose(file);
-//         create_P2_file(matrix);
-//     }
-// }
